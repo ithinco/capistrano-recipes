7e01fc636639e9f9dfc53320d33593e4771c2572
许怀灿 <huaican@me.com>
2013-08-31 01:47:18 +0800

    更新权限

--- lib/capistrano/recipes/shutdown.rb
+++ lib/capistrano/recipes/shutdown.rb
@@ -2,8 +2,8 @@
 Capistrano::Configuration.instance.load do
   namespace :shutdown do
     task :run_task, roles: :app do
-      run "rm /etc/nginx/sites-enabled/#{application}"
-      run "rm /etc/init.d/unicorn_#{application}"
+      run "#{sudo} rm /etc/nginx/sites-enabled/#{application}"
+      run "#{sudo} rm /etc/init.d/unicorn_#{application}"
     end
   end
 end
--- lib/capistrano/recipes/unicorn.rb
+++ lib/capistrano/recipes/unicorn.rb
@@ -12,8 +12,8 @@ Capistrano::Configuration.instance.load do
       template "unicorn.rb.erb", unicorn_config
       template "unicorn_init.erb", "/tmp/unicorn_init"
       run "chmod +x /tmp/unicorn_init"
-      run "#{try_sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
-      # run "#{try_sudo} update-rc.d -f unicorn_#{application} defaults"
+      run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
+      run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
     end
     after "deploy:setup", "unicorn:setup"
 
