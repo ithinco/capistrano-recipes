45c1a287da201a48718dd35fec9f3b50b2a44545
许怀灿 <huaican@me.com>
2013-10-25 14:22:23 +0800

    去掉sudo

--- lib/capistrano/recipes/unicorn.rb
+++ lib/capistrano/recipes/unicorn.rb
@@ -12,8 +12,8 @@ Capistrano::Configuration.instance.load do
       template "unicorn.rb.erb", unicorn_config
       template "unicorn_init.erb", "/tmp/unicorn_init"
       run "chmod +x /tmp/unicorn_init"
-      run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
-      run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
+      run "mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
+      # run "update-rc.d -f unicorn_#{application} defaults"
     end
     after "deploy:setup", "unicorn:setup"
 
