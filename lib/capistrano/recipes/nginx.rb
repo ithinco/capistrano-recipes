763e4940aa2e8f82e572843653416df455557766
许怀灿 <huaican@me.com>
2013-12-06 00:28:05 +0800

    update

--- lib/capistrano/recipes/nginx.rb
+++ lib/capistrano/recipes/nginx.rb
@@ -14,8 +14,8 @@ Capistrano::Configuration.instance.load do
     desc "Setup nginx configuration for this application"
     task :setup, roles: :web do
       template "nginx.conf.erb", "/tmp/nginx_conf"
-      run "mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
-      run "rm -f /etc/nginx/sites-enabled/default"
+      run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
+      run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
       # restart
     end
     after "deploy:setup", "nginx:setup"
@@ -23,7 +23,7 @@ Capistrano::Configuration.instance.load do
     %w[start stop restart].each do |command|
       desc "#{command} nginx"
       task command, roles: :web do
-        # run "#{sudo} /etc/init.d/nginx #{command}"
+        run "#{sudo} /etc/init.d/nginx #{command}"
       end
     end
   end
